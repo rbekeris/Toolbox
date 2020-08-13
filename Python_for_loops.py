a00b561d181c1ee9d47fe88c647fb53e89898a0d
rbekeris <bekeris.code@gmail.com>
2020-08-13 19:18:41 +0300

    Some more good stuff

---
+++ Python_for_loops.py
@@ -0,0 +1,14 @@
+a_dict = {'color': 'blue', 'fruit': 'apple', 'pet': 'dog'}
+
+
+#For each key in dict, print the key and value
+for key in a_dict:
+    print(key)
+    print(a_dict[key])
+
+#Return items 
+
+
+
+#For each value-key pair in dictionary, append a tuple
+
