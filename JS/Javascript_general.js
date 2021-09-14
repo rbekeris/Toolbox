42c0f3f4168ef7bcababe11f624f07b4b0663bae
rbekeris <bekeris.code@gmail.com>
2021-09-14 22:07:46 +0300

    Conditional examples added

--- JS/Javascript_general.js
+++ JS/Javascript_general.js
@@ -1,2 +1,41 @@
 //Use console
 console.log("Hello world!")
+
+//Logical Operators
+//AND
+true && false // false
+true && true // true
+//OR
+true || false // true
+false || false // false
+//NOT
+!true // false
+!false // true
+//TERNARY (if true then 1st, if false then 2nd)
+console.log(true ? 1:2) // returns 1
+console.log(false ? 1:2) // returns 2
+
+//ASSIGNMENT
+// Define a variable
+let one = 1, two = 2;
+const greeting = "Hello";
+
+//FUNCTIONS
+//IF 
+let ValueOfInterest = 3
+if (ValueOfInterest == 3) {
+	console.log("Brilliant");
+	} else { 
+		console.log("Not so Brilliant");
+	}
+
+//IF & ELSE IF
+let ValueOfInterest = 2 
+if (ValueOfInterest == 3) {
+	console.log("Brilliant");
+	} else if (ValueOfInterest == 2){
+		console.log("Almost Brilliant");
+		} else {
+			console.log("Not so Brilliant");
+		}
+
