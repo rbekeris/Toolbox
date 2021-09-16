a5db527865ff0be9be9d0782a4f13b6391c2065d
rbekeris <bekeris.code@gmail.com>
2021-09-16 23:47:25 +0300

    Added Array tools and began Object handlers

--- JS/Javascript_general.js
+++ JS/Javascript_general.js
@@ -98,3 +98,60 @@ console.log(day1.AteLunch)
 //we can easily add new property and assign a value to it!
 day1.BrushedTeeth = true;
 console.log(day1.BrushedTeeth)
+
+//ARRAY LOOPS
+let OurBrilliantArray = [2,3,4,5,6];
+for (let i = 0; i < OurBrilliantArray.length; i++) {
+	let OurArrayMember = OurBrilliantArray[i];
+	console.log("Member #:" + i + OurArrayMember);
+}
+
+//post 2015 Javascript adds a better way to write the same thing
+let OurBrilliantArray = [2,3,4,5,6];
+for (let OurArrayMember of OurBrilliantArray) {
+	console.log("Member #:" + OurArrayMember);
+}
+
+//A function that loops over array members and append a new array only with unique values
+
+function CreateUniqueMemberArray(Array){
+	let UniqueMembers = []
+	for (let MemberEntry of Array) {
+		//If the UniqueMembers don't include the iterated member...
+			if (!UniqueMembers.includes(MemberEntry)) {
+				UniqueMembers.push(MemberEntry);
+			}
+		}
+	return UniqueMembers;
+	}
+let OurBrilliantArray = [1,1,2,2,3,3,5,5,6,6,6];
+UniqueMemberArray = CreateUniqueMemberArray(OurBrilliantArray)
+console.log(UniqueMemberArray)
+
+//Return the position of a specific value in array
+let BigArray = ["Hamster","Tank","Mailbox","Bear"]
+console.log(BigArray.indexOf("Mailbox"));
+//Return the position, but start counting from the other end (maybe faster in a very large array)
+console.log(BigArray.lastIndexOf("Mailbox"));
+
+//Slice an array between two indices (start= inclusive end = exclusive)
+console.log(["Bear","Fox","Turtle","Ant","Elephant","Horse"].slice(2,4))
+//when end index is not given, returns everything after
+console.log(["Bear","Fox","Turtle","Ant","Elephant","Horse"].slice(2))
+
+//Concat + Slice = function that removes a certain element
+function remove(array, index) {
+	return array.slice(0,index).concat(array.slice(index + 1));
+}
+console.log(remove(["a","b","c","d","e"],3))
+
+//METHODS
+//Methods are Object properties that hold function values
+let rabbit = {};
+rabbit.speak = function(line) {
+	console.log(`The rabbit says '${line}'`);
+};
+rabbit.speak("I actually don't like carrots that much.")
+
+//CLASSES
+//A class defines the shape of a type of object (what methods and properties it has). An object of such class is called an Instance.
